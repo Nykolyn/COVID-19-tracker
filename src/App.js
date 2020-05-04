5989a00bbacdca6d18dba3ed748d3856916e015f
Nykolyn <nykolynandrii@gmail.com>
2020-05-04 22:41:15 +0300

    update with modern code style / set loader

--- src/App.js
+++ src/App.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 
-import { Cards, Chart, CountryPicker } from "./Components";
-import css from "./App.module.css";
+import { Cards, Chart, CountryPicker, Loader } from "./Components";
 import { fetchData } from "./api";
 
-class App extends React.Component {
-  state = {
-    data: {},
+const useStyles = makeStyles({
+  container: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+export default function App() {
+  const classes = useStyles();
+  const [data, setData] = useState({
+    confirmed: {},
+    recovered: {},
+    deaths: {},
+    lastUpdate: {},
+  });
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    setLoading(true);
+    setTimeout(async () => {
+      try {
+        const fetchedData = await fetchData();
+        console.log("fetchedData", fetchedData);
+        setData({
+          confirmed: fetchedData.confirmed,
+          recovered: fetchedData.recovered,
+          deaths: fetchedData.deaths,
+          lastUpdate: fetchedData.lastUpdate,
+        });
+        setLoading(false);
+      } catch (e) {
+        console.error("Error while getting data: ", e);
+        setLoading(false);
+      }
+    }, 1110);
   };
 
-  async componentDidMount() {
-    const fetchedData = await fetchData();
-    this.setState({ data: fetchedData });
-  }
+  if (loading) return <Loader />;
 
-  render() {
-    const { data } = this.state;
-    return (
-      <div className={css.container}>
-        <Cards data={data} />
-        <CountryPicker />
-        <Chart />
-      </div>
-    );
-  }
+  return (
+    <div className={classes.container}>
+      <Cards
+        confirmed={data.confirmed}
+        recovered={data.recovered}
+        deaths={data.deaths}
+        lastUpdate={data.lastUpdate}
+      />
+      <CountryPicker />
+      <Chart />
+    </div>
+  );
 }
-
-export default App;
--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup";
 
-import css from "./Cards.module.css";
+const CardTextContent = [
+  {
+    title: "Infected",
+  },
+  {
+    title: "Infected",
+  },
+  {
+    title: "Infected",
+  },
+];
 
-const Cards = () => {
+const Cards = ({ confirmed, recovered, deaths, lastUpdate }) => {
+  // if(!confirmed.value) return 'Loading...'
   return (
-    <div className={css.container}>
+    <div className="container">
       <Grid container spacing={3} justify="center">
         <Grid item component={Card}>
           <CardContent>
@@ -13,9 +25,40 @@ const Cards = () => {
               Infected
             </Typography>
             <Typography variant="h5">Real Data</Typography>
-            <Typography color="textSecondary">Real date</Typography>
+            <Typography color="textSecondary">
+              <CountUp
+                start={0}
+                end={confirmed.value}
+                duration={2.5}
+                separator="."
+              />
+            </Typography>
             <Typography variant="body2">
               Number of active cases of COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">{recovered.value}</Typography>
+            <Typography variant="body2">
+              Number of recoveries from COVID-19
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Deaths
+            </Typography>
+            <Typography variant="h5">Real Data</Typography>
+            <Typography color="textSecondary">{deaths.value}</Typography>
+            <Typography variant="body2">
+              Number of deaths caused by COVID-19
             </Typography>
           </CardContent>
         </Grid>
