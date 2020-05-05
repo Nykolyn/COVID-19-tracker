276b25b17cf1918e04af01041d9b0450da91bf51
Nykolyn <nykolynandrii@gmail.com>
2020-05-05 23:17:43 +0300

    Updates

---
+++ precache-manifest.01de701ec54da16264ea1c1c17555889.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cbaae73e36dec32e31d9db2524ac53b",
+    "url": "/COVID-19-tracker/index.html"
+  },
+  {
+    "revision": "243334e90a69d3d3b3b4",
+    "url": "/COVID-19-tracker/static/css/main.43c8d391.chunk.css"
+  },
+  {
+    "revision": "20d6242eda127af6742d",
+    "url": "/COVID-19-tracker/static/js/2.40489654.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/COVID-19-tracker/static/js/2.40489654.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "243334e90a69d3d3b3b4",
+    "url": "/COVID-19-tracker/static/js/main.90d7630c.chunk.js"
+  },
+  {
+    "revision": "79e21fc38eabc5a4ff9b",
+    "url": "/COVID-19-tracker/static/js/runtime-main.e97cce7b.js"
+  }
+]);
